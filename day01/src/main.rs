5dd4c44544f51af7191e7f69db96676b49bd6eb6
Malcolm Lashley <mlashley@gmail.com>
2022-12-03 17:55:23 +0000

    Day1 Part2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -5,34 +5,39 @@ use std::path::Path;
 fn main() {
 
     if let Ok(lines) = read_lines("./input.txt") {
-        let mut elf_total_calories:Vec<i32> = Vec::new();
-        // Consumes the iterator, returns an (Optional) String
-        let mut elf_count = 0;
+        
         let mut elf_max = 0;
         let mut current_elf_sum = 0;
+        let mut elf_carrying_vector = Vec::with_capacity(260); // we know this is enough from part1
+
+        // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(cal) = line {
                 let i = cal.parse::<i32>().unwrap_or(0) ;
                 current_elf_sum += i;
                 if i == 0 {
-//                        println!("{}", current_elf_sum);
-                    
-                    elf_count += 1;
+                    elf_carrying_vector.push(current_elf_sum);
                     if current_elf_sum > elf_max  {
                         elf_max = current_elf_sum;
-                        println!("Elf {} is new max {}",elf_count,current_elf_sum);
+                        println!("Debug: Elf {} is new max {}",elf_carrying_vector.len(),current_elf_sum);
                     }
                     current_elf_sum=0;
                 }
             }
         }
         // No blank line at end - check the final elf
-        elf_count += 1;
+        elf_carrying_vector.push(current_elf_sum);
         if current_elf_sum > elf_max  {
             elf_max = current_elf_sum;
-            println!("Elf {} is new max {}",elf_count,current_elf_sum);
+            println!("Elf {} is new max {}",elf_carrying_vector.len(),current_elf_sum);
         }
-        println!("Processed {} elves and max is {}",elf_count,elf_max);
+
+        let elf_count = elf_carrying_vector.len();
+        println!("Part1: Processed {} elves and max is carrying {} calories",elf_count,elf_max);
+        elf_carrying_vector.sort();
+        let top_three = elf_carrying_vector.split_off(elf_count-3);
+        println!("Part2: Top 3 elves {:?}",top_three);
+        println!("Part2: Top 3 total {} calories", top_three.into_iter().sum::<i32>());
     }
 }
 
@@ -43,4 +48,3 @@ where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
-
