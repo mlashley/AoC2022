0963178bf5e4f2ebefeb8595b8c5496c70c7ec84
Malcolm Lashley <mlashley@gmail.com>
2022-12-04 15:11:07 +0000

    Day 04 - Part1

---
+++ day04/src/main.rs
@@ -0,0 +1,82 @@
+use std::fmt;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+struct ElfPair {
+    lmin: u8,
+    lmax: u8,
+    rmin: u8,
+    rmax: u8,
+}
+
+impl ElfPair {
+    fn new(lmin: u8, lmax: u8, rmin: u8, rmax: u8) -> Self {
+        Self {
+            lmin,
+            lmax,
+            rmin,
+            rmax,
+        }
+    }
+    fn from_string(s: String) -> Self {
+        let v: Vec<&str> = s.split(',').collect();
+        let l: Vec<&str> = v[0].split('-').collect();
+        let r: Vec<&str> = v[1].split('-').collect();
+        ElfPair {
+            lmin: l[0].parse::<u8>().unwrap(),
+            lmax: l[1].parse::<u8>().unwrap(),
+            rmin: r[0].parse::<u8>().unwrap(),
+            rmax: r[1].parse::<u8>().unwrap(),
+        }
+    }
+    fn has_overlap(&self) -> bool {
+        if self.lmin <= self.rmin && self.lmax >= self.rmax {
+            return true;
+        }
+        if self.rmin <= self.lmin && self.rmax >= self.lmax {
+            return true;
+        }
+        false
+    }
+}
+
+impl fmt::Display for ElfPair {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Write strictly the first element into the supplied output
+        // stream: `f`. Returns `fmt::Result` which indicates whether the
+        // operation succeeded or failed. Note that `write!` uses syntax which
+        // is very similar to `println!`.
+        write!(f, "{}-{},{}-{}", self.lmin, self.lmax, self.rmin, self.rmax)
+    }
+}
+
+fn main() {
+    let mut soln = (0, 0);
+    if let Ok(lines) = read_lines("./input.txt") {
+        for line in lines.flatten() {
+            let elf_pair = ElfPair::from_string(line);
+            println!(
+                "ElfPair {} has overlap {}",
+                elf_pair,
+                elf_pair.has_overlap()
+            );
+            if elf_pair.has_overlap() {
+                soln.0 += 1
+            }
+        }
+    }
+
+    println!("Solutions for Part1,Part2 are {:?}", soln);
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
