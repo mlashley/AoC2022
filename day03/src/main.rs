db8afdbb1f00d67feecb80fd747d256828b301f8
Malcolm Lashley <mlashley@gmail.com>
2022-12-04 00:19:44 +0000

    Day2: Tidy ship, fix warnings

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,50 +1,47 @@
+use ascii::AsciiChar;
+use ascii::AsciiStr;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use ascii::AsciiChar;
-use ascii::AsciiStr;
 
 fn main() {
     if let Ok(lines) = read_lines("./input.txt") {
-
         let mut priority_total = 0;
         for backpack in lines.flatten() {
-            let l = backpack.len()/2;
+            let l = backpack.len() / 2;
             let mut compartment1 = backpack;
             let compartment2 = compartment1.split_off(l);
             // println!("{} {}",compartment1,compartment2);
 
-            let mask1 = strToMask(compartment1);
-            println!("Mask1 {:#b}",mask1);
-            let mask2 = strToMask(compartment2);
-            println!("Mask2 {:#b}",mask2);
+            let mask1 = str_to_mask(compartment1);
+            println!("Mask1 {:#b}", mask1);
+            let mask2 = str_to_mask(compartment2);
+            println!("Mask2 {:#b}", mask2);
 
-            println!("AND: {:#b}",mask1&mask2);
+            println!("AND: {:#b}", mask1 & mask2);
 
-            let p = get_priority(mask1&mask2);
+            let p = get_priority(mask1 & mask2);
             priority_total += p.unwrap();
-            println!("Prio: {} Total: {} ",p.unwrap(),priority_total)
-            
+            println!("Prio: {} Total: {} ", p.unwrap(), priority_total);
         }
     }
 }
 
-fn strToMask(s: String) -> u64 {
+fn str_to_mask(s: String) -> u64 {
     let ass = AsciiStr::from_ascii(&s).unwrap();
-    println!("AS: {:?} ",ass.as_bytes());
+    println!("AS: {:?} ", ass.as_bytes());
 
     let base: u64 = 2;
-    let mut mask:u64 = 0;
-    
-    for c in ass.chars() {
-        if c >= AsciiChar::A && c <= AsciiChar::Z{
-            let d = 26+c.as_byte()-AsciiChar::A.as_byte();
+    let mut mask: u64 = 0;
+
+    for ch in ass.chars() {
+        if ch >= AsciiChar::A && ch <= AsciiChar::Z {
+            let d = 26 + ch.as_byte() - AsciiChar::A.as_byte();
             mask |= base.pow(d as u32);
-        } else if c >= AsciiChar::a && c <= AsciiChar::z {
-            let d = c.as_byte()-AsciiChar::a.as_byte();
+        } else if ch >= AsciiChar::a && ch <= AsciiChar::z {
+            let d = ch.as_byte() - AsciiChar::a.as_byte();
             mask |= base.pow(d as u32);
         }
-        
     }
     // println!("i = {}", mask);
     mask
@@ -59,7 +56,6 @@ fn get_priority(i: u64) -> Option<u64> {
     }
     None
 }
-
 
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
