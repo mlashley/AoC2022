dc27f44bbf5360e282122ad4baa59e782b139551
Malcolm Lashley <mlashley@gmail.com>
2022-12-04 15:25:09 +0000

    Day 4 - Part 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -11,14 +11,6 @@ struct ElfPair {
 }
 
 impl ElfPair {
-    fn new(lmin: u8, lmax: u8, rmin: u8, rmax: u8) -> Self {
-        Self {
-            lmin,
-            lmax,
-            rmin,
-            rmax,
-        }
-    }
     fn from_string(s: String) -> Self {
         let v: Vec<&str> = s.split(',').collect();
         let l: Vec<&str> = v[0].split('-').collect();
@@ -30,11 +22,22 @@ impl ElfPair {
             rmax: r[1].parse::<u8>().unwrap(),
         }
     }
-    fn has_overlap(&self) -> bool {
+    fn has_complete_overlap(&self) -> bool {
         if self.lmin <= self.rmin && self.lmax >= self.rmax {
             return true;
         }
         if self.rmin <= self.lmin && self.rmax >= self.lmax {
+            return true;
+        }
+        false
+    }
+    fn has_partial_overlap(&self) -> bool {
+        // This does _not_ include the full-overlap case...
+        if self.lmin >= self.rmin && self.lmin <= self.rmax {
+            return true;
+        }
+
+        if self.lmax >= self.rmin && self.lmax <= self.rmax {
             return true;
         }
         false
@@ -58,16 +61,19 @@ fn main() {
         for line in lines.flatten() {
             let elf_pair = ElfPair::from_string(line);
             println!(
-                "ElfPair {} has overlap {}",
+                "ElfPair {} full-overlap {} partial-overlap {}",
                 elf_pair,
-                elf_pair.has_overlap()
+                elf_pair.has_complete_overlap(),
+                elf_pair.has_partial_overlap()
             );
-            if elf_pair.has_overlap() {
-                soln.0 += 1
+            if elf_pair.has_complete_overlap() {
+                soln.0 += 1;
+                soln.1 += 1;
+            } else if elf_pair.has_partial_overlap() {
+                soln.1 += 1;
             }
         }
     }
-
     println!("Solutions for Part1,Part2 are {:?}", soln);
 }
 
