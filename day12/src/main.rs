4fe98d785223af31f00dc4a458eda87d2c421780
Malcolm Lashley <mlashley@gmail.com>
2022-12-13 23:40:17 +0000

    Day 12 - rustfmt

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,9 +1,8 @@
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::fmt::Debug;
 use std::fs::File;
 use std::io::{self, BufRead};
-use std::collections::{HashMap, HashSet, VecDeque};
 use std::path::Path;
-
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
@@ -14,7 +13,6 @@ where
 }
 
 fn main() {
-
     // Part 1
     debug_assert!(part1("./input_sample.txt".into()) == 31);
     let part1_result = part1("./input.txt".into());
@@ -35,15 +33,11 @@ struct Coord {
 }
 impl Coord {
     fn new(x: usize, y: usize) -> Self {
-        Self {
-            x,
-            y,
-        }
+        Self { x, y }
     }
 }
 
 fn part1(input_filename: String) -> usize {
-    
     let mut mymap = Vec::new();
     if let Ok(lines) = read_lines(input_filename) {
         for line in lines.flatten() {
@@ -53,20 +47,19 @@ fn part1(input_filename: String) -> usize {
     // println!("{:?}",mymap);
     let start = match find_start(&mymap) {
         Ok(x) => x,
-        Err(y) => panic!("{}",y)
+        Err(y) => panic!("{}", y),
     };
     let end = match find_end(&mymap) {
         Ok(x) => x,
-        Err(y) => panic!("{}",y)
+        Err(y) => panic!("{}", y),
     };
-    mymap[end.y][end.x]='z';
-    mymap[start.y][start.x]='a';
+    mymap[end.y][end.x] = 'z';
+    mymap[start.y][start.x] = 'a';
 
-    solve(&mymap,start,end)
+    solve(&mymap, start, end)
 }
 
 fn part2(input_filename: String) -> usize {
-
     let mut mymap = Vec::new();
     if let Ok(lines) = read_lines(input_filename) {
         for line in lines.flatten() {
@@ -75,17 +68,17 @@ fn part2(input_filename: String) -> usize {
     }
     let start = match find_start(&mymap) {
         Ok(x) => x,
-        Err(y) => panic!("{}",y)
+        Err(y) => panic!("{}", y),
     };
     let end = match find_end(&mymap) {
         Ok(x) => x,
-        Err(y) => panic!("{}",y)
+        Err(y) => panic!("{}", y),
     };
-    mymap[end.y][end.x]='z';
-    mymap[start.y][start.x]='a';
+    mymap[end.y][end.x] = 'z';
+    mymap[start.y][start.x] = 'a';
 
-    let starts = find_all(&mymap,'a');
-    println!("Need to solve for {} starting positions...",starts.len());
+    let starts = find_all(&mymap, 'a');
+    println!("Need to solve for {} starting positions...", starts.len());
     // There is probably a smarter way to do this - but I'm a day behind and anyway:
     //
     // $ time  cargo rr
@@ -101,7 +94,7 @@ fn part2(input_filename: String) -> usize {
 
     let mut lengths = Vec::new();
     for s in starts {
-        lengths.push(solve(&mymap,s,end));
+        lengths.push(solve(&mymap, s, end));
     }
     lengths.sort();
     // println!("lengths: {:?}",lengths);
@@ -116,7 +109,7 @@ fn solve(mymap: &Vec<Vec<char>>, start: Coord, end: Coord) -> usize {
 
     let mut visited: HashSet<Coord> = HashSet::new();
     let mut q: VecDeque<Coord> = std::iter::once(start).collect();
-    let mut d: HashMap<Coord,usize> = std::iter::once((start,0)).collect();
+    let mut d: HashMap<Coord, usize> = std::iter::once((start, 0)).collect();
 
     while let Some(c) = q.pop_front() {
         // println!("Trying {:?}",c);
@@ -138,7 +131,7 @@ fn solve(mymap: &Vec<Vec<char>>, start: Coord, end: Coord) -> usize {
                 let ndist = d.get(&possibility).unwrap_or(&usize::MAX);
                 if cdist < *ndist {
                     // println!("      Pushing poss: {:?} dist: {}",possibility,cdist+1);
-                    d.insert(possibility,cdist+1);
+                    d.insert(possibility, cdist + 1);
                     q.push_back(possibility);
                 } else {
                     // println!("      Too Far {} >= {}",cdist,*ndist);
@@ -147,7 +140,7 @@ fn solve(mymap: &Vec<Vec<char>>, start: Coord, end: Coord) -> usize {
                 // println!("    {:?} is NOT a possibility ({},{})",possibility,nheight,cheight);
             }
         }
-    }   
+    }
     if d.contains_key(&end) {
         d[&end]
     } else {
@@ -157,52 +150,52 @@ fn solve(mymap: &Vec<Vec<char>>, start: Coord, end: Coord) -> usize {
 
 fn find_all(m: &[Vec<char>], c: char) -> Vec<Coord> {
     let mut v = Vec::new();
-    for (y,row)  in m.iter().enumerate() {
-        for (x,elem) in row.iter().enumerate() {
+    for (y, row) in m.iter().enumerate() {
+        for (x, elem) in row.iter().enumerate() {
             if *elem == c {
-                v.push(Coord::new(x,y));
+                v.push(Coord::new(x, y));
             }
         }
     }
     v
 }
 
-fn find_thing(m: &[Vec<char>], c: char) -> Result<Coord,String> {
-    for (y,row)  in m.iter().enumerate() {
-        for (x,elem) in row.iter().enumerate() {
+fn find_thing(m: &[Vec<char>], c: char) -> Result<Coord, String> {
+    for (y, row) in m.iter().enumerate() {
+        for (x, elem) in row.iter().enumerate() {
             if *elem == c {
-                return Ok(Coord::new(x,y))
+                return Ok(Coord::new(x, y));
             }
         }
     }
     Err("Fuck".into())
 }
 
-fn find_start(m: &[Vec<char>]) -> Result<Coord,String> {
+fn find_start(m: &[Vec<char>]) -> Result<Coord, String> {
     find_thing(m, 'S')
 }
 
-fn find_end(m: &[Vec<char>]) -> Result<Coord,String> {
+fn find_end(m: &[Vec<char>]) -> Result<Coord, String> {
     find_thing(m, 'E')
 }
 
-fn possible(m: &Vec<Vec<char>>, c: Coord ) -> Vec<Coord> {
+fn possible(m: &Vec<Vec<char>>, c: Coord) -> Vec<Coord> {
     let mut v = Vec::with_capacity(4);
 
     let height = m.len();
     let width = m[0].len();
-    
-    if c.x>0 {
-            v.push(Coord { x: c.x-1, y: c.y })
+
+    if c.x > 0 {
+        v.push(Coord { x: c.x - 1, y: c.y })
     }
-    if c.y>0 {
-            v.push(Coord { x: c.x, y: c.y-1 })
+    if c.y > 0 {
+        v.push(Coord { x: c.x, y: c.y - 1 })
     }
-    if c.x < width - 1  {
-            v.push(Coord { x: c.x+1, y: c.y })
+    if c.x < width - 1 {
+        v.push(Coord { x: c.x + 1, y: c.y })
     }
     if c.y < height - 1 {
-            v.push(Coord { x: c.x, y: c.y+1 })
+        v.push(Coord { x: c.x, y: c.y + 1 })
     }
     // println!("Adjacent to {:?} is {:?}",c,v);
     v
