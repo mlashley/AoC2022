23c05806d01725c6f9d6e13bc53a72ae8f3e8ece
Malcolm Lashley <mlashley@gmail.com>
2023-01-04 00:47:48 +0000

    Day 22 - Part 2 (A fucking mess, but the last star...)

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,5 +1,7 @@
 use itertools::Itertools;
 use std::collections::HashMap;
+use std::hash::Hash;
+use std::ops::BitAnd;
 use std::time::Instant;
 
 fn test() {
@@ -10,6 +12,9 @@ fn test() {
         map: vec![],
         instructions: String::from("1L2R3"),
         history: HashMap::new(),
+        edges: HashMap::new(),
+        foldme: Vec::new(),
+        cubesize: 5,
     };
     debug_assert!(p.password() == 6032);
 
@@ -20,7 +25,7 @@ fn test() {
                 .as_str(),
         ) == 6032
     );
-    debug_assert!(part2(std::fs::read_to_string("input.txt").unwrap().as_str(),) == 5031);
+    debug_assert!(part2(std::fs::read_to_string("input_sample.txt").unwrap().as_str(),) == 5031);
 }
 
 #[derive(Debug, Clone, Default)]
@@ -31,6 +36,9 @@ struct PlayerOne {
     map: Vec<Vec<char>>,
     instructions: String,
     history: HashMap<(i64, i64), i16>,
+    edges: HashMap<(char, u8), (char, u8)>,
+    foldme: Vec<Vec<char>>,
+    cubesize: usize,
 }
 
 #[derive(Debug, Clone)]
@@ -113,6 +121,9 @@ impl PlayerOne {
             map,
             instructions: action_string.into(),
             history: HashMap::new(),
+            edges: HashMap::new(),
+            foldme: Vec::new(),
+            cubesize: 0, // unused in part 1.
         }
     }
     fn from_str2(data: &str) -> Self {
@@ -337,10 +348,261 @@ impl PlayerOne {
         // Odd/Even => x=>y (also rotate from 'flatmap' perspective)
         // Even/Odd => y=>x (also rotate from 'flatmap' perspective)
 
-        PlayerOne::default()
+        let action_string = data.split('\n').filter(|x| x.contains('R')).last().unwrap();
+        let x = flatmap[0]
+            .iter()
+            .enumerate()
+            .find(|(_, &v)| v == '.')
+            .unwrap()
+            .0 as i64;
+
+
+        Self {
+            x,
+            y: 0,
+            facing: 90,
+            map: flatmap,
+            instructions: action_string.into(),
+            history: HashMap::new(),
+            edges,
+            foldme,
+            cubesize,
+        }
+
     }
+
+    fn get_side(&self) -> (char,u8) {
+        let small_y = self.y as usize / self.cubesize;
+        let small_x = self.x as usize / self.cubesize;
+        let side = self.foldme[small_y][small_x];
+        
+        let mut my_edges: u8 = 0;
+        if self.x as usize % self.cubesize == 0 {
+            my_edges |= 1 << 3;
+        }
+        if (self.x as usize + 1) % self.cubesize == 0 {
+            my_edges |= 1 << 1;
+        }
+        if self.y as usize % self.cubesize == 0 {
+            my_edges |= 1 << 0;
+        }
+        if (self.y as usize + 1) % self.cubesize == 0 {
+            my_edges |= 1 << 2;
+        }
+        println!("Side of {},{} => [{}.{}] is {}, Edges: {}",self.x,self.y,small_x,small_y,side,my_edges);
+
+        (side,my_edges)
+    }
+
+    fn flat_to_face(&self,x: usize ,y: usize) -> (usize,usize) {
+        let small_y = y  / self.cubesize;
+        let small_x = x  / self.cubesize;
+        let side = self.foldme[small_y][small_x];
+        let face_x = x - (small_x*self.cubesize);
+        let face_y = y - (small_y*self.cubesize);
+        println!("Global {},{} => side {} face_coord {},{}",x,y,side,face_x,face_y);
+        (face_x,face_y)
+    }
+
+    fn face_to_flat(&self,face: char,face_x: usize ,face_y: usize) -> (i64,i64 ) {
+        let mut small_x = 0;
+        let mut small_y = 0;
+        while self.foldme[small_y][small_x] != face {
+            
+            small_x += 1;
+            if small_x == self.foldme[0].len() {
+                small_x = 0;
+                small_y += 1;
+            }
+        }
+        println!("face_to_flat: Found {} at {} {}",face,small_x,small_y);
+        let flat_x = (small_x*self.cubesize)+face_x;
+        let flat_y = (small_y*self.cubesize)+face_y;
+        (flat_x as i64,flat_y as i64)
+    }
+
     fn mov2(&mut self, distance: u32) {
-        todo!()
+
+// 0123456789012345
+//           111111 
+
+//         0123       0       A
+//         1#..       1     BCD
+//         2...       2       EF
+//         3...       3
+// 012301230123       4
+// 1...1...1...       5
+// 2.#.2..#2...       6
+// 3...3...3.#.       7
+//         01230123   8
+//         1...1#..   9
+//         2#..2...  10
+//         3...3.#.  11
+
+// 0123456789012345
+//           111111 
+        
+
+        let mut count = 0;
+        while count < distance {
+
+            let (xdelta, ydelta) = match self.facing {
+                0 => (0, -1),
+                90 => (1, 0),
+                180 => (0, 1),
+                270 => (-1, 0),
+                _ => {
+                    panic!("We are the hellarwi!")
+                }
+            };
+
+            // Are we about to move off the side?
+            let mut curr_face = self.get_side();
+            let mut next_face: (char,u8);
+
+            let (fx,fy) = self.flat_to_face(self.x as usize, self.y as usize);
+
+            let mut nextx = self.x + xdelta;
+            let mut nexty = self.y + ydelta;
+            let mut next_rot = self.facing;
+
+            if curr_face.1.bitand(1)==1 && self.facing == 0 {
+                next_face = *self.edges.get(&(curr_face.0,1)).unwrap();
+                curr_face.1 = 1;
+            } else if curr_face.1.bitand(2)==2 && self.facing == 90  {
+                next_face = *self.edges.get(&(curr_face.0,2)).unwrap();
+                curr_face.1 = 2;
+            } else if curr_face.1.bitand(4)==4 && self.facing == 180 {
+                next_face = *self.edges.get(&(curr_face.0,3)).unwrap();
+                curr_face.1 = 3;
+            } else if curr_face.1.bitand(8)==8 && self.facing == 270 {
+                next_face = *self.edges.get(&(curr_face.0,4)).unwrap();
+                curr_face.1 = 4;
+            } else { 
+                next_face = curr_face;
+
+                //normal - already set above.
+                // todo!("Normal move")
+            }
+            if curr_face.0 != next_face.0 {
+                if curr_face.1 % 2 == 0 {   
+                    // curr face is even
+                    if next_face.1 % 2 == 0 {
+                        // next face is even
+                        if curr_face.1 == next_face.1 {
+                            // E/E(==) flip y and rotate 180
+                            next_rot = (((self.facing + 180) % 360) + 360) % 360; // modulo
+                            match next_face.1 {
+                                2 => (nextx,nexty) = self.face_to_flat(next_face.0, self.cubesize-1, (self.cubesize-1)-fy),
+                                4 => (nextx,nexty) = self.face_to_flat(next_face.0, 0, (self.cubesize-1)-fy ),
+                                _ => { panic!("Next side is not 2 or 4.. got {}",next_face.1)},
+                            };
+
+                            // todo!("E/E(==) flip y and rotate 180 {:?} {:?}",curr_face,next_face);
+
+                        } else {
+                            // E/E(!=) works as flat
+                        }
+                    } else { 
+                        // next face is odd
+                        // E/O(!=) y=> x and rotate -90 
+                        let (rot,nfx,nfy) = 
+                        match (curr_face.1,next_face.1) {
+                            (4,1) => (-90,fy,                  0),
+                            (2,3) => (-90,fy,                  self.cubesize-1),
+                            (4,3) => ( 90,(self.cubesize-1)-fy,self.cubesize-1),
+                            (2,1) => ( 90,(self.cubesize-1)-fy,0),
+                            _ => panic!("Funny old E/O != pair? {:?}, {:?}", curr_face,next_face)
+                        };
+                        next_rot = (((self.facing+rot) % 360) + 360) % 360; // modulo
+                        println!("nfx,y {},{} [fx, fy {},{}]",nfx,nfy,fx,fy);
+                        (nextx,nexty) = self.face_to_flat(next_face.0, nfx, nfy);
+                        println!("E/O(!=) y=>x and rotate {:?} {:?}",curr_face,next_face);
+                    }
+
+                } else {
+                    // curr face is odd
+                    if next_face.1 % 2 == 0 {
+                        // next face is even 
+                        // O/E(!=) x=>y and rotate +90
+                        next_rot = (((self.facing + 90) % 360) + 360) % 360; // modulo
+                        match next_face.1 {
+                            2 => (nextx,nexty) = self.face_to_flat(next_face.0, self.cubesize-1, fx),
+                            4 => (nextx,nexty) = self.face_to_flat(next_face.0, 0, fx),
+                            _ => { panic!("Next side is not 2 or 4.. got {}",next_face.1)},
+                        };
+                        // todo!("O/E(!=) x=>y and rotate {:?} {:?}",curr_face,next_face);
+                        
+                    } else { 
+                        // next face is odd
+                        if curr_face.1 == next_face.1 {
+                            // O/O(==) flip y and rotate 180
+                            next_rot = (((self.facing + 180) % 360) + 360) % 360; // modulo
+                            match next_face.1 {
+                                1 => (nextx,nexty) = self.face_to_flat(next_face.0, (self.cubesize-1)-fx, 0),
+                                3 => (nextx,nexty) = self.face_to_flat(next_face.0, (self.cubesize-1)-fx, self.cubesize-1),
+                                _ => { panic!("Next side is not 1 or 3.. got {}",next_face.1)},
+                            };
+
+                            // todo!("Odd to Odd equal")
+                        } else {
+                            // O/O(!=) works as flat - except when on opposite sides...
+                            match next_face.1 {
+                                1 => (nextx,nexty) = self.face_to_flat(next_face.0, fx, 0),
+                                3 => (nextx,nexty) = self.face_to_flat(next_face.0, fx, self.cubesize-1),
+                                _ => { panic!("Next side is not 1 or 3.. got {}",next_face.1)},
+                            };
+                        }
+                        
+                    }
+
+                }
+                
+            }
+        // Odd/Odd(!=) and Even/Even(!=) edges all stay flat (1-3, 2-4 etc)
+        // Even/Even(equal) edge-pairs (2-2, 4-4) are flipped so the y0 => ymax and ymax => y0 as they cross:  y=>ymax-y 180deg rotate from flatmap perspective.
+        // Odd/Odd(equal) - dunno?
+        // Odd/Even => x=>y (also rotate from 'flatmap' perspective)
+        // Even/Odd => y=>x (also rotate from 'flatmap' perspective)
+
+
+            // if nexty < 0 {
+            //     nexty = self.map.len() as i64 - 1
+            // } // wrap top around to bottom
+            // if nextx < 0 {
+            //     nextx = self.map[0].len() as i64 - 1
+            // } // wrap
+            // if nexty == self.map.len() as i64 {
+            //     nexty = 0
+            // } // wrap
+            // if nextx == self.map[0].len() as i64 {
+            //     nextx = 0
+            // } // wrap
+
+            println!("NextX,Y is {},{} looking for space",nextx,nexty);
+
+
+            assert!(nextx >= 0);
+            assert!(nexty >= 0);
+
+            while match self.map[nexty as usize][nextx as usize] {
+                '.' => {
+                    self.x = nextx;
+                    self.y = nexty;
+                    self.facing = next_rot;
+                    false
+                }
+                '#' => false,
+                ' ' => {
+                    panic!("Attempt to read the void map space at nextx,y = {},{}",nextx,nexty);
+                }
+                _ => {
+                    panic!["Map is corrupt :("]
+                }
+            } {}
+            self.history.insert((self.x, self.y), self.facing);
+            count += 1;
+        }
     }
 
     fn rot(&mut self, deg: i16) {
@@ -465,12 +727,17 @@ fn part2(data: &str) -> i64 {
 
     println!("PLAYER: {:?}", player);
 
+    player.print_map();
+    assert!(player.get_side() == ('A',9));
+
     while let Some(ins) = Instruction::next(&mut player.instructions) {
         println!("INS: {:?}", ins);
         match ins {
             Instruction::Rotate(r) => player.rot(r),
             Instruction::Forward(d) => player.mov2(d),
         }
+        player.print_map();
+
     }
     player.print_map();
     player.password()
@@ -482,8 +749,8 @@ fn main() {
     let p1 = part1(std::fs::read_to_string("input.txt").unwrap().as_str());
     println!("Part1: {}", p1);
     assert!(p1 == 76332);
-    // let p2 = part2(std::fs::read_to_string("input.txt").unwrap().as_str());
-    // println!("Part2: {}", p2);
-    // assert!(p2 as i64 == ?);
+    let p2 = part2(std::fs::read_to_string("input.txt").unwrap().as_str());
+    println!("Part2: {}", p2);
+    assert!(p2 as i64 == 144012);
     println!("Completed in {} us", now.elapsed().as_micros());
 }
